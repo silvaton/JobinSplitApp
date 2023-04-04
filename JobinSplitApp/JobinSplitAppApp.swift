3abba9f32bc9ab29753b6b098ceda87679f654c8
Ton Silva <tdasilva@ext.laligatech.com>
2023-04-04 13:28:47 +0200

    Initial commit

---
+++ JobinSplitApp/JobinSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobinSplitAppApp.swift
+//  JobinSplitApp
+//
+//  Created by Ton Silva on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct JobinSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
